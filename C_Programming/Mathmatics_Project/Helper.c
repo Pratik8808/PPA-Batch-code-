263d1c45b3cf80bef815a1a7e393941efd1eb69f
Pratik Narule <pratiknarule88@gmail.com>
2026-04-22 20:45:30 +0530

    22 april 2026 Pre processor Leraned and Directives

---
+++ C_Programming/Mathmatics_Project/Helper.c
@@ -0,0 +1,12 @@
+
+//Function che defination
+
+int Addition (int A,int B)
+{
+    return A+B;
+}
+
+int Subtraction (int A,int B)
+{
+    return A-B;
+}
---
+++ C_Programming/Test.c
@@ -0,0 +1,20 @@
+// Demostration of pre processor
+
+/*
+  
+Pre Placement Activity
+  22 April 2026
+*/
+
+#include <stdio.h>
+
+#define PI 3.14
+int main ()
+{
+    // Area of the Circle
+    float Area =0.0f;
+    Area=10.5f*10.5f*PI;
+
+    printf("Area of circle is : %f\n",Area);
+
+}
