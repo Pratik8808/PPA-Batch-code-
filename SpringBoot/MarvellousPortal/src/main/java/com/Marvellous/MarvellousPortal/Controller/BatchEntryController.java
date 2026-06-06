fb00cbc63443db59cb500016f313e4565e3313f0
Pratik Narule <pratiknarule88@gmail.com>
2026-06-06 16:56:45 +0530

    4 june 2026

--- SpringBoot/MarvellousPortal/src/main/java/com/Marvellous/MarvellousPortal/Controller/BatchEntryController.java
+++ SpringBoot/MarvellousPortal/src/main/java/com/Marvellous/MarvellousPortal/Controller/BatchEntryController.java
@@ -1,6 +1,45 @@
 package com.Marvellous.MarvellousPortal.Controller;
 
+
+import com.Marvellous.MarvellousPortal.Entity.BatchEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/Batches")
 public class BatchEntryController
 {
+  private HashMap<Long , BatchEntry> batchentries=new HashMap<Long , BatchEntry>();
+
+
+  //Select * from Batches
+  @GetMapping
+  public ArrayList<BatchEntry>getAll()
+  {
+     return new ArrayList<>(batchentries.values());
+  }
+
+
+  //Insert into Batches (1,"PPA",28000);
+  @PostMapping
+    public String CreateEntry(@RequestBody BatchEntry myEntry)
+  {
+      batchentries.put(myEntry.getId(),myEntry);
+      return "Data inserted Sucessfully";
+  }
+
+  @DeleteMapping
+    public void  deleteEntryById(@PathVariable Long myid)
+  {
+      batchentries.remove(myid);
+  }
+
+  @PutMapping("/id/{myid}")
+    public void updateEntryById(@PathVariable Long myid,@RequestBody BatchEntry myEntry)
+  {
+      batchentries.put(myEntry.getId(),myEntry);
+  }
 
 }
